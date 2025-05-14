65484130789cc02101965c3c3d27c76d29cf5fb7
magnusmenger <magnus@magnusmenger.de>
2025-05-14 21:18:51 +0200

    Start implementing variation of rod constraints

---
+++ src/RodConstraintVar.cpp
@@ -0,0 +1,49 @@
+#include "RodConstraintVar.h"
+
+#if defined(__APPLE__) && defined(__aarch64__)
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+RodConstraintVar::RodConstraintVar(Particle *p1, Particle *p2, double dist, size_t index) : m_p1(p1), m_p2(p2), m_dist(dist), Constraint(index) {}
+
+std::vector<JacobianEntry> RodConstraintVar::getJacobian() {
+    double dist = getC() + m_dist;
+    return {JacobianEntry{m_p1, (m_p1->m_Position[0] - m_p2->m_Position[0]) / dist,
+                          (m_p1->m_Position[1] - m_p2->m_Position[1]) / dist},
+            JacobianEntry{m_p2, - (m_p1->m_Position[0] - m_p2->m_Position[0]) / dist,
+                          - (m_p1->m_Position[1] - m_p2->m_Position[1]) / dist}};
+}
+
+std::vector<JacobianEntry> RodConstraintVar::getJacobianDeriv() {
+    double dist = getC() + m_dist;
+    double interim = getCDeriv() * dist;
+    return {JacobianEntry{m_p1, - interim * (m_p1->m_Position[0] - m_p2->m_Position[0]) / pow(dist, 3) + (m_p1->m_Velocity[0] - m_p2->m_Velocity[0]) / dist,
+                          - interim * (m_p1->m_Position[1] - m_p2->m_Position[1]) / pow(dist, 3) + (m_p1->m_Velocity[1] - m_p2->m_Velocity[1]) / dist},
+        JacobianEntry{m_p2, interim * (m_p1->m_Position[0] - m_p2->m_Position[0]) / pow(dist, 3) - (m_p1->m_Velocity[0] - m_p2->m_Velocity[0]) / dist,
+                          interim * (m_p1->m_Position[1] - m_p2->m_Position[1]) / pow(dist, 3) - (m_p1->m_Velocity[1] - m_p2->m_Velocity[1]) / dist}};
+}
+
+double RodConstraintVar::getC() {
+    return sqrt(pow(m_p1->m_Position[0] - m_p2->m_Position[0], 2) +
+        pow(m_p1->m_Position[1] - m_p2->m_Position[1], 2)) - m_dist;
+}
+
+double RodConstraintVar::getCDeriv() {
+    double dist = getC() + m_dist;
+    return ((m_p1->m_Position[0] - m_p2->m_Position[0]) * (m_p1->m_Velocity[0] - m_p2->m_Velocity[0])
+        + (m_p1->m_Position[1] - m_p2->m_Position[1]) * (m_p1->m_Velocity[1] - m_p2->m_Velocity[1]))
+        / dist;
+}
+
+
+
+void RodConstraintVar::draw() {
+    glBegin(GL_LINES);
+    glColor3f(0.8, 0.7, 0.6);
+    glVertex2f(m_p1->m_Position[0], m_p1->m_Position[1]);
+    glColor3f(0.8, 0.7, 0.6);
+    glVertex2f(m_p2->m_Position[0], m_p2->m_Position[1]);
+    glEnd();
+}
